dcdd89cbe9c15b3c9779d3a0a770b2eb2d5828ad
William Santos <williamdsw@outlook.com>
2020-10-24 22:24:26 -0300

    Code refactoring for Carousel component

--- src/app/carousel/carousel-routing.module.ts
+++ src/app/carousel/carousel-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { CarouselComponent } from './carousel.component';
 
 const ROUTES: Routes = [
